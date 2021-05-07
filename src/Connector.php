8e90a14bd8b9c811932321f2325a70de224ad713
Björn Meyer <bjoern.meyer@flagbit.de>
2021-05-07 14:56:08 +0200

    add defined definition for HUBSPOT_KEY

--- src/Connector.php
+++ src/Connector.php
@@ -7,7 +7,7 @@ class Connector
     public function getNewConnection(): \Flagbit\Hubspot\Factory
     {
         return new \Flagbit\Hubspot\Factory([
-            'key'      => HUBSPOT_KEY
+            'key'      => defined('HUBSPOT_KEY') ? HUBSPOT_KEY : ''
         ]);
     }
 }
